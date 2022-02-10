c8222ceb23ac1aa4c52828335ed8d033df0e901e
AlexeyN <Leha.negodin@mail.ru>
2022-02-10 15:10:50 +0400

    v1 skript

---
+++ ubuntu-setup-ansible_vm1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This file should be sourced
+
+# Change directory to user home
+cd /home/ansibleadmin
+
+# Upgrade all packages that have available updates and remove old ones.
+sudo apt-get update
+sudo apt upgrade -y
+sudo apt autoremove --assume-yes
+
+# Install git
+sudo apt install git --assume-yes
+
+# Install azcli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
+# Install jenkins
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install jenkins
+
+
+# Install venv and pip
+sudo apt install python3-venv --assume-yes
+sudo apt install python3-pip --assume-yes
+
+# Setup virtual environment and push home folder ownership
+sudo python3 -m venv venv
+sudo chown ansibleadmin /home/ansibleadmin --recursive
+
+# Install ansible and azure modules into virtual environment
+pip3 install -r https://raw.githubusercontent.com/globalbao/terraform-azurerm-ansible-linux-vm/master/scripts/requirements-ansible.txt
+pip3 install -r https://raw.githubusercontent.com/globalbao/terraform-azurerm-ansible-linux-vm/master/scripts/requirements-azure.txt
