b8ad18fe2d046384ae11594005919765235388b1
AlexeyN <Leha.negodin@mail.ru>
2022-02-18 09:56:00 +0400

    docker on vm2

---
+++ ubuntu-setup-docker_vm2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This file should be sourced
+
+# Upgrade all packages that have available updates and remove old ones.
+sudo apt-get update
+sudo apt upgrade -y
+sudo apt autoremove --assume-yes
+
+# Install git
+sudo apt install git --assume-yes
+
+# Install docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
