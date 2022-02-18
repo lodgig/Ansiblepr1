0a77aa7268741ce2b28cb485f581d29e08cbd184
AlexeyN <Leha.negodin@mail.ru>
2022-02-18 10:44:36 +0400

    update

--- ubuntu-setup-docker_vm2.sh
+++ ubuntu-setup-docker_vm2.sh
@@ -13,6 +13,6 @@ sudo apt install git --assume-yes
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
+sudo apt update -y
 apt-cache policy docker-ce
 sudo apt install docker-ce
